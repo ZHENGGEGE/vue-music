7fd02c3d0cce138a27b7bdb347bc4b3392e3b07c
ZHENGGEGE <593012736@qq.com>
2017-06-22 23:01:48 +0800

    vueapp

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+   <div>hellow word</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style  scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable.styl"
+  #app
+    color : $color-theme
+</style>
