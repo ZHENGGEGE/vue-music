5ad5e5a6b03e5ec9126792f99b7fd61c49e929ef
ZHENGGEGE <593012736@qq.com>
2017-07-10 11:33:16 +0800

    listview

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -68,9 +68,15 @@
           for(let key in map){
               let val = map[key]
             if(val.title.match(/[a-zA-Z]/)){
-
+                ret.push(val)
+            }else if(val.title === HOT_NAME){
+                hot.push(val)
             }
           }
+          ret.sort((a,b) => {
+              return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+          })
+          return hot.concat(ret)
         }
       }
     }
