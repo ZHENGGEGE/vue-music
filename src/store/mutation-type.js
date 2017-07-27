3a46e911ba89cce44be5279f2323216eda9974d8
ZHENGGEGE <593012736@qq.com>
2017-07-27 22:00:17 +0800

    mutations

---
+++ src/store/mutation-type.js
@@ -0,0 +1,4 @@
+/**
+ * Created by suzheng on 17/7/27.
+ */
+export const SET_SINGER = 'SET_SINGER'
---
+++ src/store/state.js
@@ -0,0 +1,10 @@
+/**
+ * Created by suzheng on 17/7/27.
+ */
+const state = {
+  singer: {
+
+  }
+}
+
+export default state
