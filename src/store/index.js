e03c9c62e1202bd9dc7a77063607798f6b5b7ad7
ZHENGGEGE <593012736@qq.com>
2017-07-27 22:23:04 +0800

    index

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,20 @@
 /**
  * Created by suzheng on 17/7/7.
  */
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as actions from './actions'
+import * as getters from './getters'
+import state from './state'
+import mutations from './mutations'
+
+Vue.use(Vuex)
+export default new Vuex.Store({
+  actions,
+  getters,
+  mutations,
+  state
+})
+
+
+
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,3 +1,10 @@
 /**
  * Created by suzheng on 17/7/22.
  */
+import * as types from './mutation-types'
+
+const mutations = {
+  [types.SET_SINGER](state,singer){
+    state.singer = singer
+  }
+}
