471e7ae71578d95539043addf2c86d90b76458a5
ZHENGGEGE <593012736@qq.com>
2017-08-07 22:04:13 +0800

    scrollY

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -83,6 +83,7 @@
               this.scrollY = pos.y
           },
           _scrollTo(index){
+            this.scrollY = -this.listHeight[index]
             this.$refs.listview.scrollToElement(this.$refs.listGroup[index],0)
           },
           _calculateHeight(){
@@ -105,16 +106,23 @@
             },
           scrollY(newY) {
             const listHeight = this.listHeight
+            //当滚动到顶部，newY>0
+            if(newY>0){
+              this.currentIndex = 0
+              return
+            }
+            //在中间部分滚动
             for(let i = 0;i<listHeight.length;i++){
                 let height1 = listHeight[i]
                 let height2 = listHeight[i+1]
-              if(!height2 || (-newY>height1 &&-newY<height2)){
+              if (-newY>=height1 &&-newY<height2){
                 this.currentIndex = i
                 //console.log(this.currentIndex)
                 return
               }
             }
-            this.currentIndex = 0
+            //当滚动到底部，且-newY大于最后一个元素的上限
+            this.currentIndex = listHeight.length - 2
           }
         },
         components:{
