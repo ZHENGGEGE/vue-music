08a4ba1330620ac76489cca29020cf8eb9c79441
ZHENGGEGE <593012736@qq.com>
2017-09-04 00:04:26 +0800

    song类的封装

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -11,3 +11,28 @@ export default class Song {
 
     }
 }
+
+export function createSong(musicData){
+    return new Song({
+        id : musicData.id,
+        mid : musicData.mid,
+        singer : filterSinger(musicData.singer),
+        name : musicData.name,
+        album : musicData.album,
+        duration : musicData.interval,
+        image : `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
+        url : `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+    })
+}
+
+
+function filterSinger(singer){
+    let ret = []
+    if(!singer){
+        return ''
+    }
+    singer.forEach((s)=>{
+        ret.push(s.name)
+    })
+    return ret.join('/')
+}
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -10,8 +10,14 @@
   import { mapGetters } from 'vuex'
   import {getSingerDetail} from 'api/singer'
   import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
 
   export default {
+    data() {
+      return {
+        songs:[]
+      }
+    },
     computed: {
       ...mapGetters([
         'singer'
@@ -20,8 +26,6 @@
     created() {
       this._getDetail()
       console.log(this.singer)
-
-
 
     },
     methods:{
@@ -32,10 +36,22 @@
         }
         getSingerDetail(this.singer.id).then((res)=>{
             if(res.code === ERR_OK){
-                console.log(res.data.list)
+                //console.log(res.data.list)
+                this.songs = this._normalizeSongs(res.data.list)
+                console.log(this.songs)
             }
         })
 
+      },
+      _normalizeSongs(list){
+          let ret = []
+          list.forEach((item)=>{
+            let {musicData} = item
+            if(musicData.songid && musicData.albummid){
+              ret.push(createSong(musicData))
+            }
+          })
+          return ret
       }
     }
   }
