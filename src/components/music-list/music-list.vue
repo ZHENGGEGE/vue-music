103555e13a7ec2e20a962a527a4bb78017aa0c83
ZHENGGEGE <593012736@qq.com>
2017-09-17 14:56:44 +0800

    下拉图片放大

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -5,7 +5,7 @@
       </div>
       <h1 class="title" v-html="title"></h1>
       <div class="bg-image" :style="bgStyle" ref="bgImage">
-          <div class="filter"></div>
+          <div class="filter" ref="filter"></div>
       </div>
       <div class="bg-layer" ref="layer">
 
@@ -68,8 +68,19 @@ export default {
     scrollY(newY) {
       let translateY = Math.max(this.minTranslateY,newY)
       let zIndex = 0
+      let scale = 1
+      let blur = 0
       this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
       this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)`
+      const percent = Math.abs(newY/this.imageHeight)
+      if(newY>0){
+        scale = 1 + percent
+        zIndex = 10
+      }else{
+        blur = Math.min(20*percent,20)
+      }
+      this.$refs.filter.style['backdrop-filter']=`blur(${blur})`
+      this.$refs.filter.style['webkitBackdrop-filter']=`blur(${blur})`
       if(newY<this.minTranslateY){
         zIndex = 10
         this.$refs.bgImage.style.paddingTop = 0
@@ -79,6 +90,8 @@ export default {
         this.$refs.bgImage.style.height = 0
       }
       this.$refs.bgImage.style.zIndex = zIndex
+      this.$refs.bgImage.style['transform']=`scale(${scale})`
+      this.$refs.bgImage.style['webkitTransform']=`scale(${scale})`
     }
   },
   components:{
