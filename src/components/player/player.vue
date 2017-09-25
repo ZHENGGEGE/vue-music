2e6f0602f40b257938c789979879b987b4c5f41d
ZHENGGEGE <593012736@qq.com>
2017-09-25 09:04:44 +0800

    animation

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,64 +1,71 @@
 <template>
   <div class="player" v-show="playlist.length>0">
-      <div class="normal-player" v-show="fullScreen">
-          <div class="background">
-              <img :src="currentSong.image" alt="" height="100%" width="100%">
-          </div>
-          <div class="top">
-              <div class="back">
-                  <i class="icon-back"></i>
-              </div>
-              <h1 class="title" v-html="currentSong.name"></h1>
-              <h2 class="subtitle" v-html="currentSong.singer"></h2>
-          </div>
-          <div class="middle">
-              <div class="middle-l">
-                  <div class="cd-wrapper">
-                      <div class="cd">
-                          <img :src="currentSong.image" alt="" class="image">
-                      </div>
-                  </div>
-              </div>
-          </div>
-          <div class="bottom">
-              <div class="operators">
-                  <div class="icon i-left">
-                      <i class="icon-sequence"></i>
-                  </div>
-                  <div class="icon i-left">
-                      <i class="icon-prev"></i>
-                  </div>
-                  <div class="icon i-center">
-                      <i class="icon-play"></i>
-                  </div>
-                  <div class="icon i-right">
-                      <i class="icon-next"></i>
-                  </div>
-                  <div class="icon i-right">
-                      <i class="icon icon-not-favorite"></i>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div class="mini-player" v-show="!fullScreen">
-          <div class="icon">
-              <img :src="currentSong.image" alt="" width="40" height="40">
-          </div>
-          <div class="text">
-              <h2 class="name" v-html="currentSong.name"></h2>
-              <p class="desc" v-html="currentSong.singer"></p>
-          </div>
-          <div class="coltrol">
-              
-          </div>
-          <div class="control">
-            <i class="icon-playlist"></i>
-          </div>
-      </div>
+      <transition name="normal" @enter="enter" 
+                                @after-enter="afterEnter" 
+                                @leave="leave" 
+                                @after-leave="leaveEnter">  
+        <div class="normal-player" v-show="fullScreen">
+            <div class="background">
+                <img :src="currentSong.image" alt="" height="100%" width="100%">
+            </div>
+            <div class="top">
+                <div class="back" @click="back">
+                    <i class="icon-back"></i>
+                </div>
+                <h1 class="title" v-html="currentSong.name"></h1>
+                <h2 class="subtitle" v-html="currentSong.singer"></h2>
+            </div>
+            <div class="middle">
+                <div class="middle-l">
+                    <div class="cd-wrapper">
+                        <div class="cd">
+                            <img :src="currentSong.image" alt="" class="image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bottom">
+                <div class="operators">
+                    <div class="icon i-left">
+                        <i class="icon-sequence"></i>
+                    </div>
+                    <div class="icon i-left">
+                        <i class="icon-prev"></i>
+                    </div>
+                    <div class="icon i-center">
+                        <i class="icon-play"></i>
+                    </div>
+                    <div class="icon i-right">
+                        <i class="icon-next"></i>
+                    </div>
+                    <div class="icon i-right">
+                        <i class="icon icon-not-favorite"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </transition>
+      <transition name="mini">
+        <div class="mini-player" v-show="!fullScreen" @click="open">
+            <div class="icon">
+                <img :src="currentSong.image" alt="" width="40" height="40">
+            </div>
+            <div class="text">
+                <h2 class="name" v-html="currentSong.name"></h2>
+                <p class="desc" v-html="currentSong.singer"></p>
+            </div>
+            <div class="coltrol">
+                
+            </div>
+            <div class="control">
+                <i class="icon-playlist"></i>
+            </div>
+        </div>
+      </transition>
   </div>
 </template>
 <script>
-import {mapGetters} from 'vuex'
+import {mapGetters,mapMutations} from 'vuex'
 export default {
   computed : {
       ...mapGetters([
@@ -66,6 +73,29 @@ export default {
           'playlist',
           'currentSong'
       ])
+  },
+  methods : {
+      back(){
+          this.setFullScreen(false)
+      },
+      open(){
+           this.setFullScreen(true)
+      },
+      enter(el,done){
+
+      },
+      afterEnter(){
+
+      },
+      leave(){
+
+      },
+      afterLeave(){
+
+      },
+      ...mapMutations({
+          setFullScreen : 'SET_FULL_SCREEN'
+      })   
   }
 }
 </script>
