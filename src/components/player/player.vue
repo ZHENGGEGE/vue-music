d6560e1859f6d0bb77b60e0a6161919ff2388396
ZHENGGEGE <593012736@qq.com>
2017-10-15 20:19:56 +0800

    歌词滚动

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -23,6 +23,16 @@
                         </div>
                     </div>
                 </div>
+                <scroll class="middle-r" ref="lyricList" :data="currentLyric&&currentLyric.lines">
+                    <div class="lyric-wrapper">
+                      <div v-if="currentLyric">
+                        <p class="text" 
+                           ref="lyricLine"
+                           :class="{'current': currentLineNum === index}"
+                           v-for="(line,index) in currentLyric.lines">{{line.txt}}</p>
+                      </div>
+                    </div>
+                </scroll>
             </div>
             <div class="bottom">
                 <div class="progress-wrapper">
@@ -85,6 +95,7 @@ import ProgressCircle from 'base/progress-circle/progress-circle'
 import {playMode} from 'common/js/config'
 import {shuffle} from 'common/js/util'
 import Lyric from 'lyric-parser'
+import Scroll from 'base/scroll/scroll'
 
 const transform = prefixStyle('transform')
 
@@ -94,7 +105,8 @@ export default {
       songReday : false,
       currentTime : 0,
       radius : 32,
-      currentLyric : null
+      currentLyric : null,
+      currentLineNum : 0
     }
   },
   computed : {
@@ -221,9 +233,21 @@ export default {
       },
       getLyric(){
         this.currentSong.getLyric().then((lyric) => {
-          this.currentLyric = new Lyric(lyric)
+          this.currentLyric = new Lyric(lyric,this.handleLyric)
+          if(this.playing){
+            this.currentLyric.play()
+          }
           console.log(this.currentLyric)
         })
+      },
+      handleLyric({lineNum,txt}){
+        this.currentLineNum = lineNum
+        if(lineNum > 5){
+          let lineEl = this.$refs.lyricLine[lineNum-5]
+          this.$refs.lyricList.scrollToElement(lineEl,1000)
+        }else{
+          this.$refs.lyricList.scrollTo(0,0,1000)
+        }
       },
       _pad(num,n=2){
         let len = num.toString().length
@@ -310,7 +334,8 @@ export default {
   },
   components : {
     ProgressBar,
-    ProgressCircle
+    ProgressCircle,
+    Scroll
   }
 }
 </script>
