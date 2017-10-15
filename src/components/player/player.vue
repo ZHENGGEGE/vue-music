d6d91f6fe5d25f3ed7ec3259de18de911e2dfd75
ZHENGGEGE <593012736@qq.com>
2017-10-16 00:07:06 +0800

    歌词滑动

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -15,8 +15,11 @@
                 <h1 class="title" v-html="currentSong.name"></h1>
                 <h2 class="subtitle" v-html="currentSong.singer"></h2>
             </div>
-            <div class="middle">
-                <div class="middle-l">
+            <div class="middle"
+                  @touchstart.prevent="middleTouchStart"
+                  @touchmove.prevent="middleTouchMove"
+                  @touchend="middleTouchEnd">
+                <div class="middle-l" ref="middleL">
                     <div class="cd-wrapper" ref="cdWrapper">
                         <div class="cd" :class="cdCls">
                             <img :src="currentSong.image" alt="" class="image">
@@ -35,6 +38,10 @@
                 </scroll>
             </div>
             <div class="bottom">
+                <div class="dot-wrapper">
+                    <span class="dot" :class="{'active':currentShow === 'cd'}"></span>
+                    <span class="dot" :class="{'active':currentShow === 'lyric'}"></span>
+                </div>
                 <div class="progress-wrapper">
                   <span class="time time-l">{{format(currentTime)}}</span>
                   <div class="progress-bar-wrapper">
@@ -98,6 +105,7 @@ import Lyric from 'lyric-parser'
 import Scroll from 'base/scroll/scroll'
 
 const transform = prefixStyle('transform')
+const transitionDuration = prefixStyle('transitionDuration')
 
 export default {
   data(){
@@ -106,7 +114,8 @@ export default {
       currentTime : 0,
       radius : 32,
       currentLyric : null,
-      currentLineNum : 0
+      currentLineNum : 0,
+      currentShow : 'cd'
     }
   },
   computed : {
@@ -137,6 +146,9 @@ export default {
           'mode',
           'sequenceList'
       ])
+  },
+  created (){
+    this.touch = {}
   },
   methods : {
       back(){
@@ -306,6 +318,59 @@ export default {
       loop(){
         this.$refs.audio.currentTime = 0
         this.$refs.audio.play()
+      },
+      middleTouchStart(e){
+        this.touch.initiated = true
+        const touch = e.touches[0]
+        this.touch.startX = touch.pageX
+        this.touch.startY = touch.pageY      
+      },
+      middleTouchMove(e){
+        if(!this.touch.initiated){
+          return
+        }
+        const touch = e.touches[0]
+        const deltaX = touch.pageX - this.touch.startX
+        const deltaY = touch.pageY - this.touch.startY
+        if(Math.abs(deltaY)>Math.abs(deltaX)){
+          return
+        }
+        const left = this.currentShow === 'cd' ? 0 : -window.innerWidth
+        const offsetWidth = Math.min(0,Math.max(-window.innerWidth,left+deltaX))
+        this.touch.precent = Math.abs(offsetWidth/window.innerWidth)
+        this.$refs.lyricList.$el.style[transform] = `translate3d(${offsetWidth}px,0,0)`
+        this.$refs.lyricList.$el.style[transitionDuration] = 0
+        this.$refs.middleL.style.opaticy = 1 - this.touch.precent
+        this.$refs.middleL.style[transitionDuration] = 0
+      },
+      middleTouchEnd(){
+        let offsetWidth
+        let opacity
+        if(this.currentShow === 'cd'){
+          if(this.touch.precent > 0.1){
+            offsetWidth = -window.innerWidth
+            this.currentShow = 'lyric'
+            opacity = 0
+          }else{
+            offsetWidth = 0
+            opacity = 1
+          }
+        }else{
+          if(this.touch.precent < 0.9){
+            offsetWidth = 0
+            this.currentShow = 'cd'
+            opacity = 1
+          }else{
+            offsetWidth = -window.innerWidth
+            opacity = 0
+          }
+        }
+        const time = 300
+        this.$refs.lyricList.$el.style[transform] = `translate3d(${offsetWidth}px,0,0)`
+        this.$refs.lyricList.$el.style[transitionDuration] = `${time}ms`
+        this.$refs.middleL.style.opacity = opacity
+        this.$refs.middleL.style[transitionDuration] = `${time}ms`
+
       },
       ...mapMutations({
           setFullScreen : 'SET_FULL_SCREEN',
